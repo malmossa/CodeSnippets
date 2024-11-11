a5318ec08548b8c89655bd9bf7805dd28d0d93d4
Mansor Almossa <malmossa@gmail.com>
2024-11-10 17:19:09 -0800

    Setup is completed

--- CodeSnippets/Models/Snippet.cs
+++ CodeSnippets/Models/Snippet.cs
@@ -1,6 +1,28 @@
-﻿namespace CodeSnippets.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeSnippets.Models
 {
     public class Snippet
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null;
+
+        [Required]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        public string? IdentityUserId { get; set; }
+
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? User {  get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedDate { get; set; }
+
     }
 }
